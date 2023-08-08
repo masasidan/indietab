47cd780e09b4cb053c2cfb80b520eb962b3ee94e
Idan Masas <idanmasasofficial@gmail.com>
2023-08-08 23:02:32 +0300

    Update page.tsx

--- app/u/[username]/page.tsx
+++ app/u/[username]/page.tsx
@@ -23,7 +23,7 @@ const User = ({ params: { username } }: IUser) => {
         <UserCard username={username} />
         <div className="mt-24 mb-5 hidden lg:block">
           <p className="text-gray-500 text-sm">
-            Want an indie card of your own?{" "}
+            Want an indie tab of your own?{" "}
             <Link href="" className="text-red-500 font-medium hover:underline">
               Create one now
             </Link>
@@ -32,7 +32,7 @@ const User = ({ params: { username } }: IUser) => {
         </div>
         <div className="flex flex-col items-center justify-center gap-3 absolute bottom-3 rounded-xl right-0 left-0 w-[95%] mx-auto p-6 bg-red-500/40 backdrop-blur-[3px] lg:hidden">
           <p className="text-white text-lg text-center">
-            Want an indie card of your own?
+            Want an indie tab of your own?
           </p>
           <Link
             href=""
